352fb0797422b60bbae3c965eceb809e03502532
sankapapa <pavankumar8485@gmail.com>
2023-09-09 18:43:13 +0530

    only on testing branch

--- WheatTask.java
+++ WheatTask.java
@@ -2,7 +2,7 @@ public class WheatTask implements Runnable {
 
     @Override
     public void run() {
-        System.out.println("I am doing  task in Wheattask class");
+        System.out.println("I am doing  task in Wheattask class-only on testing branch");
     }
     
 }
